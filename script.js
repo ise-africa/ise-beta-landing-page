f7d9e153c3aa8d3f693a75d749a80cc992130b33
Tobe0504 <ezimorahtobenna@gmail.com>
2023-11-08 13:22:36 +0100

    added the timer and e books container

--- script.js
+++ script.js
@@ -106,3 +106,47 @@ const openSideNav = () => {
 const closeSideNav = () => {
   sideNav.style.width = "0%";
 };
+
+const date = new Date();
+
+function countdownToLaunch(targetDate) {
+  const now = new Date().getTime();
+
+  const timeRemaining = targetDate - now;
+
+  if (timeRemaining <= 0) {
+    return "Launch date has passed!";
+  }
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  return { days: days, hours: hours, minutes, minutes, seconds: seconds };
+}
+
+const launchDate = new Date("2023-12-20T00:00:00").getTime();
+const countdown = countdownToLaunch(launchDate);
+
+const daysDiv = document.querySelector(".countdown > div:nth-child(1) > h2");
+const hourssDiv = document.querySelector(".countdown > div:nth-child(3) > h2");
+const minutesDiv = document.querySelector(".countdown > div:nth-child(5) > h2");
+const secondssDiv = document.querySelector(
+  ".countdown > div:nth-child(7) > h2"
+);
+
+const startTimer = () => {
+  setInterval(() => {
+    daysDiv.innerHTML = countdownToLaunch(launchDate).days;
+    hourssDiv.innerHTML = countdownToLaunch(launchDate).hours;
+    minutesDiv.innerHTML = countdownToLaunch(launchDate).minutes;
+    secondssDiv.innerHTML = countdownToLaunch(launchDate).seconds;
+  }, 1000);
+};
+
+startTimer();
+
+console.log(countdown);
